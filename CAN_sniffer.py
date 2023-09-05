5fa6f50fe62e5054d63fe30140d0848b135e8105
Marcin Zapolski <mz4pol@gmail.com>
2023-09-05 14:35:06 +0200

    Major update
    Update to python3 syntax.
    Replace eblib and com_monitor thread with just pyserial.
    Add interpretation and visualization of IR data

--- CAN_sniffer.py
+++ CAN_sniffer.py
@@ -1,196 +1,187 @@
-from PyQt4 import QtCore, QtGui
 
-from ui_MainWin import Ui_MainWindow
-from eblib.serialutils import full_port_name, enumerate_serial_ports
-import Queue
-from com_monitor import ComMonitorThread
-from eblib.utils import get_all_from_queue, get_item_from_queue
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtGui import QColor
+from ui_MainWindow import Ui_MainWindow
+import queue
+import serial
+import time
+import struct
+import numpy as np
+import argparse
 
+ROWS = 4
+COLS = 16
+TOTAL_PIXELS = ROWS * COLS
+MIN_COLOR = QColor.fromRgbF(0, 0, 1)  # Blue for the minimum temperature
+MAX_COLOR = QColor.fromRgbF(1, 0, 0)  # Red for the maximum temperature
 
-class CAN_Sniffer(QtGui.QMainWindow):
-    def __init__(self, parent=None):
+class CAN_Sniffer(QtWidgets.QMainWindow):
+    def __init__(self, parent=None, port=None):
         super(CAN_Sniffer, self).__init__(parent)
-
         self.ui = Ui_MainWindow()
-
         self.ui.setupUi(self)
+        self.serial = None
+        self.rcv_buffer = bytearray()
+        self.messages = {}
+        self.ir_data = np.zeros(TOTAL_PIXELS, dtype=np.int16)
 
-        self.monitor_active = False
-        self.com_monitor = None
-        self.com_data_q = None
-        self.com_error_q = None
-        self.data = ""
-        # self.livefeed = LiveDataFeed()
-        # self.temperature_samples = []
+        self.data_q = queue.Queue()
+        self.error_q = queue.Queue()
         self.timer = QtCore.QTimer()
 
+        self.timer.timeout.connect(self.read_data)
         self.ui.serialPortSelectPb.clicked.connect(self.on_select_port)
-        self.ui.serialConnectPb.clicked.connect(self.on_start)
-        self.ui.serialDisconnectPb.clicked.connect(self.on_stop)
-        self.ui.serialConnectPb.setEnabled(False)
-        self.ui.serialDisconnectPb.hide()
 
-    # @QtCore.pyqtSlot(int)
-    # def on_inputSpinBox1_valueChanged(self, value):
-    #     self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox2.value()))
-
-    # @QtCore.pyqtSlot(int)
-    # def on_inputSpinBox2_valueChanged(self, value):
-    #     self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox1.value()))
-
-        self.ui.dataTable.setColumnWidth(0,50)
-        self.ui.dataTable.setColumnWidth(1,50)
-        # for i in range(2,10):
-        #     self.ui.dataTable.setColumnWidth(i,25)
-
-        # self.update_data(512, [2,3,4,5,6,7,8,9])
-        # self.update_data(654, [1,1,1,1,1,1,1,1])
-        # self.update_data(543, [6,8,4,8,2,6,5,8])
-        # self.update_data(456, [4,5,7,9,23,7,9,3])
-        # self.update_data(512, [5,8,2,78,9,2,78,8])
-        # self.update_data(512, [3,78,9,23,78,8,3])
-        # self.update_data(512, [3,78,10,23,78])
+        if port is not None:
+            self.connect_to_port(port)
 
     def on_select_port(self):
-        ports = list(enumerate_serial_ports())
-        if len(ports) == 0:
-            QMessageBox.critical(self, 'No ports',
-                'No serial ports found')
-            return
+        import serial.tools.list_ports
+        port_list = [port.device for port in serial.tools.list_ports.comports()]
+        if self.serial is not None:
+            self.serial.close()
+            self.timer.stop()
+        port, ok = QtWidgets.QInputDialog.getItem(self, 'Select Port', 'Available Serial Ports:', port_list, 0, False)
         
-        item, ok = QtGui.QInputDialog.getItem(self, 'Select a port',
-                    'Serial port:', ports, 0, False)
-        
-        if ok and not item.isEmpty():
-            self.ui.serialConnectPb.setEnabled(True)
-            self.ui.serialPortLbl.setText(item)            
-            # self.set_actions_enable_state()
-
-    def on_stop(self):
-        """ Stop the monitor
-        """
-        if self.com_monitor is not None:
-            self.com_monitor.join(0.01)
-            self.com_monitor = None
-
-        self.monitor_active = False
-        self.timer.stop()
-        # self.set_actions_enable_state()
-        
-        self.ui.statusbar.showMessage('Monitor idle')
-        self.ui.serialConnectPb.show()
-        self.ui.serialDisconnectPb.hide()
-
+        if ok and port:
+            self.connect_to_port(port)
     
-    def on_start(self):
-        """ Start the monitor: com_monitor thread and the update
-            timer
-        """
-        if self.com_monitor is not None or self.ui.serialPortLbl.text() == '':
+    def connect_to_port(self, port):
+        self.ui.serialPortSelectPb.setText(port)
+        try:
+            self.serial = serial.Serial(port, 921600, timeout=0, exclusive=True)
+            self.timer.start(100)
+            self.ui.statusbar.showMessage(f'Connected to {port}')
+        except serial.SerialException as e:
+            self.ui.statusbar.showMessage(str(e))
+
+    def read_data(self):
+        while self.serial.in_waiting:
+            self.rcv_buffer.extend(self.serial.read(size=1024))
+            
+        while b'RCV' in self.rcv_buffer:
+            start_idx = self.rcv_buffer.index(b'RCV')
+            end_idx = start_idx + 3 + 20  # 'RCV' length (3) + CanRxMsg struct length (20)
+            
+            if len(self.rcv_buffer) >= end_idx:
+                msg_data = self.rcv_buffer[start_idx+3:end_idx]
+                CanRxMsg = struct.unpack('<LLBBB8sB', msg_data)
+                # print(f'CanRxMsg: {CanRxMsg}')
+                self.update_data(CanRxMsg)
+                self.rcv_buffer = self.rcv_buffer[end_idx:]
+            else:
+                break
+
+    def update_ir_data(self, std_id, new_data):
+        if 0x100 <= std_id <= 0x10F:
+            index = (std_id - 0x100) * 4
+            self.ir_data[index:index + 4] = new_data
+
+    def update_data(self, CanRxMsg):
+        std_id, ext_id, ide, rtr, dlc, data, fmi = CanRxMsg
+        new_message = True
+        
+        self.update_ir_data(std_id, struct.unpack('<4h', data[:8]))
+        self.ui.tMinLabel.setText(f'{np.min(self.ir_data)/10:.1f}°C')
+        self.ui.tMaxLabel.setText(f'{np.max(self.ir_data)/10:.1f}°C')
+        self.ui.tAvgLabel.setText(f'{np.mean(self.ir_data)/10:.1f}°C')
+        self.update_ir_image()
+
+        if ide == 0x00000000:
+            msg_id = std_id
+        elif ide == 0x00000004:
+            msg_id = ext_id
+        else:
+            print(f"Unhandled IDE value: 0x{ide:08X}")
             return
-        
-        self.data_q = Queue.Queue()
-        self.error_q = Queue.Queue()
-        self.com_monitor = ComMonitorThread(
-            self.data_q,
-            self.error_q,
-            full_port_name(str(self.ui.serialPortLbl.text())),
-            38400)
-        self.com_monitor.start()
-        
-        com_error = get_item_from_queue(self.error_q)
-        if com_error is not None:
-            QMessageBox.critical(self, 'ComMonitorThread error',
-                com_error)
-            self.com_monitor = None
 
-        self.monitor_active = True
-        # self.set_actions_enable_state()
-        
-        # self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.on_timer)
-        # self.connect(self.timer, SIGNAL('timeout()'), self.on_timer)
-        
-        update_freq = 100
-        if update_freq > 0:
-            self.timer.start(1000.0 / update_freq)
-        
-        self.ui.statusbar.showMessage('Monitor running')
-        self.ui.serialConnectPb.hide()
-        self.ui.serialDisconnectPb.show()
+        row_position = -1
+        # Check if the ID already exists in the table
+        for row in range(self.ui.dataTable.rowCount()):
+            table_id = self.ui.dataTable.item(row, 0).text()
+            if table_id == f'0x{msg_id:X}':
+                row_position = row
+                new_message = False
+                break
 
+        # If ID not found, add a new row
+        if row_position == -1:
+            row_position = self.ui.dataTable.rowCount()
+            self.ui.dataTable.insertRow(row_position)
+            self.ui.dataTable.setItem(row_position, 0, QtWidgets.QTableWidgetItem(f'0x{msg_id:X}'))
+            self.ui.dataTable.setItem(row_position, 1, QtWidgets.QTableWidgetItem("0"))  # Count
+
+        # Update the count
+        count_item = self.ui.dataTable.item(row_position, 1)
+        new_count = int(count_item.text()) + 1
+        count_item.setText(str(new_count))
+
+        # Update the data bytes (B0 to B7)
+        data_bytes = CanRxMsg[5]
+        for i in range(8):
+            new_value = f'{data_bytes[i]:2X}'
+            try:
+                old_value = self.ui.dataTable.item(row_position, 2 + i).text()
+            except AttributeError:
+                old_value = None
+
+            item = QtWidgets.QTableWidgetItem(new_value)
+
+            if i >= dlc:
+                item.setBackground(QtGui.QColor(200, 200, 200))
+            elif new_message or new_value != old_value:
+                item.setBackground(QtGui.QColor(255, 255, 0))
+            self.ui.dataTable.setItem(row_position, 2 + i, item)
+
+    def update_ir_image(self):
+        if not np.all(self.ir_data == 0):
+            reshaped_data = self.ir_data.reshape((COLS, ROWS))
+            # rotate 90 degrees
+            reshaped_data = np.rot90(reshaped_data, 3)
+            height, width = reshaped_data.shape
+            min_val = np.min(reshaped_data)
+            max_val = np.max(reshaped_data)
+            rgb_data = np.zeros((height, width, 3), dtype=np.uint8)
+
+            for y in range(height):
+                for x in range(width):
+                    normalized_temp = (reshaped_data[y, x] - min_val) / (max_val - min_val)
+                    color = QColor.fromRgbF(
+                        MIN_COLOR.redF() + normalized_temp * (MAX_COLOR.redF() - MIN_COLOR.redF()),
+                        MIN_COLOR.greenF() + normalized_temp * (MAX_COLOR.greenF() - MIN_COLOR.greenF()),
+                        MIN_COLOR.blueF() + normalized_temp * (MAX_COLOR.blueF() - MIN_COLOR.blueF())
+                    )
+                    rgb_data[y, x, 0] = color.red()
+                    rgb_data[y, x, 1] = color.green()
+                    rgb_data[y, x, 2] = color.blue()
+
+            image = QtGui.QImage(rgb_data.data, width, height, QtGui.QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap.fromImage(image)
+            self.ui.irImage.setScene(QtWidgets.QGraphicsScene())
+            self.ui.irImage.scene().addPixmap(pixmap)
+            self.ui.irImage.fitInView(self.ui.irImage.sceneRect(), QtCore.Qt.KeepAspectRatio)
+
+    def read_errors(self):
+        errors = False
+        while not self.error_q.empty():
+            error_msg = self.error_q.get()
+            self.ui.statusbar.showMessage(error_msg)
+            print(error_msg)
+            errors = True
+        return errors
     
-    def on_timer(self):
-        """ Executed periodically when the monitor update timer
-            is fired.
-        """
-        self.read_serial_data()
-        # self.update_monitor()
-
-    def read_serial_data(self):
-        """ Called periodically by the update timer to read data
-            from the serial port.
-        """
-        qdata = list(get_all_from_queue(self.data_q))
-        if len(qdata) > 0:
-            data = self.data+''.join(qdata)
-            while data.find("Id: ")!=-1:
-                msgStart = data.find("Id: ")
-                msgEnd = data.find("\n",msgStart)
-                if msgEnd == -1:
-                    break
-
-                packet = data[msgStart:msgEnd-1]
-                # print "msg: [%s]" % packet
-                msgId = int(packet[4:8],16)
-                # print "msgId: %d [%x]" % (msgId, msgId)
-                msgData = map(lambda x: int(x,16) ,packet[16:].split(" "))
-                # print "data: ", msgData
-                self.update_data(msgId, msgData)
-
-                data = data[msgEnd:]
-            self.data = data
-
-    def update_data(self, msgId, msgData):
-        rowCount = self.ui.dataTable.rowCount()
-        print rowCount
-        msgIdExists = False
-        for i in range(rowCount):
-            if self.ui.dataTable.item(i, 0).text() == "%04X" % msgId:
-                msgIdExists = True
-                count = int(self.ui.dataTable.item(i, 1).text())
-                self.ui.dataTable.item(i, 1).setText(str(count+1))
-                for j in range(8):
-                    try:
-                        txt = "%02X" % msgData[j]
-                    except IndexError:
-                        txt = ""
-                    if self.ui.dataTable.item(i, 2+j).text() != txt:
-                        self.ui.dataTable.item(i, 2+j).setText(txt)
-                        self.ui.dataTable.item(i, 2+j).setBackground(QtGui.QColor('red'))
-                    else:
-                        self.ui.dataTable.item(i, 2+j).setBackground(QtGui.QColor('white'))
-
-
-
-        if not msgIdExists:
-            self.ui.dataTable.insertRow(rowCount)
-            print "msgId: %d [%x]" % (msgId, msgId)
-            item = QtGui.QTableWidgetItem("%04X" % msgId)
-            self.ui.dataTable.setItem(rowCount,0,item)
-            item = QtGui.QTableWidgetItem("1")
-            self.ui.dataTable.setItem(rowCount,1,item)
-            for i in range(8):
-                try:
-                    item = QtGui.QTableWidgetItem("%02X" % msgData[i])
-                except IndexError:
-                    item = QtGui.QTableWidgetItem("")
-                self.ui.dataTable.setItem(rowCount,2+i,item)
+    def closeEvent(self, event):
+        if self.serial is not None:
+            self.serial.close()
+        event.accept()
 
 if __name__ == '__main__':
     import sys
+    parser = argparse.ArgumentParser(description='CAN Sniffer')
+    parser.add_argument('--port', '-p', type=str, help='Serial port to use')
+    args = parser.parse_args()
 
-    app = QtGui.QApplication(sys.argv)
-    sniffer = CAN_Sniffer()
-    sniffer.show()
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = CAN_Sniffer(port=args.port)
+    myapp.show()
     sys.exit(app.exec_())
